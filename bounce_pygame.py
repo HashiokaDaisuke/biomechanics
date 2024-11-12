a310c07e1af466eee770a8ff07a259bcbbf6c0f7
HashiokaDaisuke <dadasappy.1999.2.12@gmail.com>
2024-11-12 22:38:56 +0900

    円筒面を転がる小球を描画

--- bounce_pygame.py
+++ bounce_pygame.py
@@ -1,17 +1,23 @@
 import random
 import pygame
+import math
 
 
 class Particle:
-    def __init__(self, pos, vel):
+    def __init__(self, init_pos, vel, theta,r):
         self.is_alive = True
-        self.x, self.y = pos
-        self.vx, self.vy = vel
+        self.x, self.y = 0,0
+        self.init_x, self.init_y = init_pos
+        self.v = vel
+        self.theta = theta
+        self.r = r
 
-    def update(self, width, height, dt, gy):
-        self.vy += gy * dt
-        self.x += self.vx * dt
-        self.y += self.vy * dt
+    def update(self, width, height, dt, g):
+        self.g_theta = - g * math.sin(math.radians(self.theta))
+        self.v += self.g_theta * dt
+        self.theta += self.v / self.r * 180 / math.pi
+        self.x = self.init_x + self.r * math.sin(math.radians(self.theta))
+        self.y = self.init_y - self.r * (1 - math.cos(math.radians(self.theta)))
         if self.x < 0 or self.x > width or self.y > height:
             self.is_alive = False
 
@@ -27,7 +33,10 @@ def main():
     clock = pygame.time.Clock()
 
     dt = 1.0
-    gy = 0.5
+    g = 0.5
+    theta = -90
+    r = 100
+    v = 0
     particle_list = []
 
     while True:
@@ -44,15 +53,13 @@ def main():
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
 
-                    vx = random.uniform(-10, 10)
-                    vy = random.uniform(-10, 0)
-                    p = Particle(event.pos, (vx, vy))
+                    p = Particle((150,350), v, theta ,r)
                     particle_list.append(p)
         if should_quit:
             break
 
         for p in particle_list:
-            p.update(width, height, dt, gy)
+            p.update(width, height, dt, g)
 
         particle_list[:] = [p for p in particle_list if p.is_alive]
 
